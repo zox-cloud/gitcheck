fb7f6c55e05f3ca05a020973e7ffe31f8b15180d
asdf <asdf@gmail.com>
2024-08-19 17:32:17 +0500

    new value

--- src/main/java/com/example/AuthTry/config/NewPLayer.java
+++ src/main/java/com/example/AuthTry/config/NewPLayer.java
@@ -2,5 +2,13 @@ package com.example.AuthTry.config;
 
 public class NewPLayer {
     String name;
+    String surname;
 
+    @Override
+    public String toString() {
+        return "NewPLayer{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
